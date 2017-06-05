037c0ae030dad2b09379533cd4ed73cef1c1e624
gogunleye <gogunleye@ssl247.co.uk>
2017-06-05 13:15:46 +0100

    new commit

---
+++ src/test/java/PageFactory/DriverLoad.java
@@ -0,0 +1,41 @@
+package PageFactory;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class DriverLoad {
+	public WebDriver driver;
+	
+	
+
+  @BeforeTest (groups = {"Sanity"})
+  public void beforeTest() throws IOException  {
+	  
+	  System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+	  driver = new ChromeDriver();  
+	  
+  }
+
+  @AfterTest (groups = {"Sanity"})
+  public void afterTest() throws Exception {
+	  
+	  Thread.sleep(10000);
+		 //report.endTest(test);
+		 
+	driver.quit();
+	  
+  }
+
+  
+  
+}
