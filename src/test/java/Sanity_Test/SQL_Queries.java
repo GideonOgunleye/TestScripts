db4cf1ddb7ed645a3745c20bbed08c40eeb43be4
gogunleye <gogunleye@ssl247.co.uk>
2017-07-26 17:24:31 +0100

    Added Create Account To Admin Test Scenarios

--- src/test/java/Sanity_Test/SQL_Queries.java
+++ src/test/java/Sanity_Test/SQL_Queries.java
@@ -37,7 +37,9 @@ public class SQL_Queries {
 			//Query to Execute		
 			//String query = "select *  from bakewell_staging_go.accounts Where account_code ='QUAL017';";	
 			String query1 = "delete from bakewell_automation.accounts Where account_code ='QUAL017';";
-			String query2 = "delete from bakewell_staging_go.accounts Where account_code ='QUAL017';";
+			String query2 = "delete from bakewell_automation.accounts Where account_code ='SSLT006';";
+			
+			
 	      
 		    //Load mysql jdbc driver		
 	 	    Class.forName("com.mysql.jdbc.Driver");			
